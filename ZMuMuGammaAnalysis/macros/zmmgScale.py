ecbae464df524ffeb6b997495221650df8bd21d2
Pasquale Musella <pasquale.musella@cern.ch>
2014-02-18 02:40:04 +0100

    Version used for e-scales fit

---
+++ ZMuMuGammaAnalysis/macros/zmmgScale.py
@@ -0,0 +1,326 @@
+#!/bin/env python
+
+from optparse import OptionParser, make_option
+import sys, os
+import array
+from math import sqrt, fabs
+
+from zmmgPlot import ytitle
+
+objs = []
+
+def getCatName(icat, labels=None):
+    if labels:
+        return labels[icat]
+    return "cat%d" % igroup
+
+def setStat(h,prev=None,vert=-1,horiz=0.):
+    ROOT.gPad.Update()
+    st = h.FindObject('stats')
+    print st, h, prev
+    st.SetLineColor(h.GetLineColor())
+    st.SetTextColor(h.GetLineColor())
+
+    if prev:
+        shiftx = (prev.GetX2NDC() - st.GetX1NDC())*horiz
+        shifty = (prev.GetY2NDC() - st.GetY1NDC())*vert
+
+        st.SetX1NDC(st.GetX1NDC()+shiftx)
+        st.SetX2NDC(st.GetX2NDC()+shiftx)
+
+        st.SetY1NDC(st.GetY1NDC()+shifty)
+        st.SetY2NDC(st.GetY2NDC()+shifty)
+
+    ROOT.gPad.Update()
+    return st
+
+def trMean(hist,alpha):
+
+    beta = 1.-alpha
+    probs = array.array('d',[beta*0.5,1.-beta*0.5])
+    quantiles = array.array('d',[0.,0.])
+
+    hist.GetQuantiles(2,quantiles,probs)
+
+    print quantiles, probs
+    
+    limits = [ hist.GetXaxis().FindBin(q) for q in quantiles ]
+    hist.GetXaxis().SetRange(*limits)
+    
+    mean = hist.GetMean()
+    err  = hist.GetMeanError() ## hist.GetRMS() / ( ( 1. - beta )*sqrt( hist.Integral() ) )
+        
+    hist.GetXaxis().SetRange()
+    return mean,err
+
+def winMean(hist,rng):
+
+    limits = [ hist.GetXaxis().FindBin(q) for q in rng ]
+    hist.GetXaxis().SetRange(*limits)
+    
+    mean = hist.GetMean()
+    err  = hist.GetMeanError()
+        
+    hist.GetXaxis().SetRange()
+    return mean,err
+
+def getMean(h,method):
+    if type(method) == float:
+        print method
+        x,xerr = trMean(h,method)
+    elif type(method) == tuple:
+        x,xerr = winMean(h,method)
+    else:
+        x,xerr = method(h)
+
+    return x,xerr
+
+def recFit(h):
+    f = ROOT.TF1("f","gaus",80.,100.)
+    oldmean = 90.
+    oldsigma = 10.
+    
+    h.Fit(f,"QRN")
+    errmean  = f.GetParError(1)
+    mean  = f.GetParameter(1)
+    sigma = f.GetParameter(2)*1
+    iter = 0
+    while iter < 5 or fabs(1. - mean/oldmean) > 0.005:
+        g = ROOT.TF1("g","gaus",mean-sigma,mean+sigma)
+        h.Fit(g,"QRN")
+        oldmean = float(mean)
+        oldsigma = float(sigma)
+        errmean  = g.GetParError(1)
+        mean  = g.GetParameter(1)
+        sigma = g.GetParameter(2)*1
+        iter+=1
+        ## print iter, mean, oldmean, sigma
+
+    fcanv = ROOT.TCanvas("fit_%s" % h.GetName(),"fit_%s" % h.GetName())
+    fcanv.cd()
+    g = ROOT.TF1("final_fit_%s" % h.GetName(),"gaus",mean-sigma,mean+sigma)
+    g.SetLineColor(h.GetLineColor())
+    h.Fit(g,"QRO")
+    ## h.GetListOfFunctions().Add(g.Clone())
+    h.Draw()
+    g.Draw("same")
+    objs.append(g)
+    objs.append(fcanv)
+    objs.append(h)
+    
+    for fmt in ["png"]:
+        fcanv.SaveAs( "%s.%s" % ( fcanv.GetName(), fmt ) )
+                     
+    return mean,errmean
+        
+
+def toStr(method):
+    if type(method) == float:
+        return "%d" % (100.*method)
+    elif type(method) == tuple:
+        return "%1.2g_%1.2g" % method
+    else:
+        return method.__name__
+
+def buildCalib(hmcs,name,title,method):
+    
+    gr = ROOT.TGraphErrors()
+    gr.SetName(name)
+    gr.SetTitle(title)
+    
+    for y in sorted(hmcs.keys()):
+        x,xerr = getMean(hmcs[y],method)
+        ip = gr.GetN()
+        gr.SetPoint(ip,x,y)
+        gr.SetPointError(ip,xerr,0.)
+        
+    return gr
+    
+def main(options,args):
+
+    infile = args[0]
+    fin = ROOT.TFile.Open(infile)
+    os.chdir(options.outdir)
+
+    ROOT.gStyle.SetOptFit(1)
+
+    results = []
+    ## methods = [1,0.95,0.8,0.683,0.5,0.4,0.3,0.2]
+    ## methods = [recFit,0.683,(85.,100.),(80.,100.),(86.,96.),(88.,94.)]
+    ## methods = [recFit,1.,0.683,(85.,97.),(86.,96.),(88,94),(89,93),(90,92)]
+    methods = [recFit]
+    if len(options.groups) > 0:
+        categories = [ [int(t) for t in g.split(",")] for g in options.groups ]
+    else:
+        categories = [ [i] for i in range(options.ncat) ]
+    for igroup,group in enumerate(categories):
+
+        catName = getCatName(igroup,options.labels)
+        hdata = fin.Get("th1f_data_mass_cat%d" % group[0]).Clone("data_%s" % catName)
+
+        hmcs = {}
+        hmcs[0] = fin.Get("th1f_sig_dymm_mass_m90_cat%d" % group[0]).Clone("mc_%s" % catName)
+        
+        for isig in range(1,options.nsigma+1):
+            hmcs[options.step*float(isig)] = fin.Get("th1f_sig_dymm_mass_m90_cat%d_E_scaleUp0%d_sigma" % (group[0],isig) ).Clone("mc_%s_Up0%d" % (catName,options.step*float(isig)*10.))
+            hmcs[-options.step*float(isig)] = fin.Get("th1f_sig_dymm_mass_m90_cat%d_E_scaleDown0%d_sigma" % (group[0],isig) ).Clone("mc_%s_Down0%d" % (catName,options.step*float(isig)*10.))
+
+        for icat in group[1:]:
+            hdata.Add( fin.Get("th1f_data_mass_cat%d" % icat) )
+
+            hmcs[0].Add(fin.Get("th1f_sig_dymm_mass_m90_cat%d" % icat))
+        
+            for isig in range(1,options.nsigma+1):
+                hmcs[options.step*float(isig)].Add(fin.Get("th1f_sig_dymm_mass_m90_cat%d_E_scaleUp0%d_sigma" % (icat,isig) ))
+                hmcs[-options.step*float(isig)].Add(fin.Get("th1f_sig_dymm_mass_m90_cat%d_E_scaleDown0%d_sigma" % (icat,isig) ))
+                
+
+        icat = igroup
+        hdata.SetLineColor(ROOT.kBlack)
+        hdata.SetMarkerColor(ROOT.kBlack)
+        ytitle( hdata, "Events / %(binw)s GeV" )
+        for s,h in hmcs.iteritems():
+            h.SetLineColor(ROOT.kRed)
+            ytitle( h, "Events / %(binw)s GeV" )
+        hmcs[0].SetLineColor(ROOT.kBlue)
+        
+        ## hmcs[0].GetXaxis().SetRangeUser(80.,100.)
+        ROOT.gStyle.SetOptFit(1)
+        res = {}
+        for method in methods:
+            name = "mVsDeltaE_%s" % toStr(method)
+                
+            data = ( getMean(hdata,method), getMean(hmcs[0],method) )
+            res[method] = ( buildCalib(hmcs,name,";m_{ll#gamma} (GeV/c^{2});#Delta E_{#gamma} / E_{#gamma} (#times 10^{-2})", method ),
+                            data )
+
+        ROOT.gStyle.SetOptFit(0)
+        hdata.GetListOfFunctions().Clear()
+        for s,h in hmcs.iteritems():
+            h.GetListOfFunctions().Clear()
+
+        canv = ROOT.TCanvas(catName, catName )
+        canv.cd()
+
+        norm = hdata.GetMaximum() / hmcs[0].GetMaximum() * hmcs[0].Integral()
+        hmcs[0].Rebin(4)
+        hdata.Rebin(4)
+        h = hmcs[0].DrawNormalized("hist",norm)
+        ytitle( h, "Events / %(binw)1.1g GeV" )
+        st = setStat(h)
+        objs.extend((st,h))
+        
+        hdata.SetLineColor(ROOT.kBlack)
+        hdata.SetMarkerColor(ROOT.kBlack)
+        hdata.Draw("e sames")
+        objs.append(setStat(hdata,st,0.,-1))
+
+        ip = 1
+        for s,h in hmcs.iteritems():
+            if fabs(s)  != 0.4:
+                continue
+            h.Rebin(4)
+            h.SetLineColor(ROOT.kRed)
+            if s < 0:
+                h.SetLineStyle(ROOT.kDashed)
+            h = h.DrawNormalized("histsames",norm)
+            q = setStat(h,st,-1.*ip)
+            objs.extend([q,h])
+            ip += 1
+        objs.append( (canv, hdata, hmcs ))
+        
+        for fmt in "C","png","pdf":
+            canv.SaveAs("%s.%s" % ( canv.GetName(), fmt) )
+        
+        results.append(res)
+        
+    objs.append(results)
+
+    meas = {}
+    out = open("results.txt","w+")
+    for method in methods:
+        scales = []
+        name = "results_%s" % toStr(method)
+        canv = ROOT.TCanvas(name,name,2800,2000)
+        canv.Divide(options.ncat/2,2)
+        objs.append(canv)
+        for icat,res in enumerate(results):
+            canv.cd(icat+1)
+            ROOT.gPad.SetGridx()
+            ROOT.gPad.SetGridy()
+            calib, data = res[method]
+            data,mc = data
+            calib.Fit("pol1","W+")
+            func = calib.GetListOfFunctions().At(0)
+            scale = func.Eval(data[0])
+            err = sqrt(data[1]**2 + mc[1]**2)
+            scaleP = func.Eval(data[0]+err)
+            scaleM = func.Eval(data[0]-err)
+            scales.append( (data[0],mc[0],100.*(data[0]/mc[0]-1.),100.*(err/mc[0]),0.5*(scaleP-scaleM),(data[0]-mc[0])/err,scale,scaleM,scaleP) )
+            ip = calib.GetN()
+            calib.SetPoint( ip, data[0], scale )
+            calib.SetPointError( ip, err, 0.5*(scaleP-scaleM) )
+            calib.SetMarkerStyle(10)
+            calib.Draw("ap")
+        for fmt in "C","png","pdf":
+            canv.SaveAs("%s.%s" % (canv.GetName(),fmt) )
+            
+        out.write( "Method: %s\n" % toStr(method ) )
+        for icat, scale in enumerate(scales):
+            catName = getCatName(icat,options.labels)
+            out.write(("cat: %s " % catName).ljust(25) )
+            for num in scale:
+                out.write(("%.4g" % num).ljust(10) )
+            out.write("\n")
+    out.close()
+    out = open("results.txt")
+    print out.read()
+    
+        
+if __name__ == "__main__":
+    parser = OptionParser(option_list=[
+        make_option("-s", "--step",
+                    action="store", type="float", dest="step",
+                    default=0.2,
+                    ),
+        make_option("-N", "--nsigma",
+                    action="store", type="int", dest="nsigma",
+                    default=4,
+                    ),
+        make_option("-n", "--ncat",
+                    action="store", type="int", dest="ncat",
+                    default=8,
+                    ),
+        make_option("-g", "--group", 
+                    action="append",  dest="groups",
+                    default=[],
+                    ),
+        make_option("-l", "--label",
+                    action="append", dest="labels",
+                    default=[],
+                    ),
+        make_option("-o", "--outdir",
+                    action="store", dest="outdir", type="string",
+                    default="zmmgScale",
+                    ),
+        ])
+    
+    (options, args) = parser.parse_args()
+    print options
+    if len(options.labels) == 0:
+        options.labels = None
+    ## options.groups.extend( [ "0,1", "2,3", "4,5", "6,7" ] )
+    ## options.groups.extend( [ "0,4", "1,5", "2,6", "3,7" ] )
+    ## options.groups.extend( [ "0,1,4,5", "2,3,6,7" ] )
+
+    try:
+        os.mkdir(options.outdir)
+    except:
+        pass
+
+    print options
+    
+    sys.argv.append("-b")
+    import ROOT
+    
+    main( options, args )
--- ZMuMuGammaAnalysis/src/ZMuMuGammaAnalysis.cc
+++ ZMuMuGammaAnalysis/src/ZMuMuGammaAnalysis.cc
@@ -14,10 +14,15 @@
 
 using namespace std;
 
-ZMuMuGammaAnalysis::ZMuMuGammaAnalysis(){}
+ZMuMuGammaAnalysis::ZMuMuGammaAnalysis()
+{
+	phoPtMin = 20.;
+	dEtaMin = 0.;
+	applyPhoPresel = false;
+}
 ZMuMuGammaAnalysis::~ZMuMuGammaAnalysis(){}
 
-ZMuMuGammaAnalysis::TreeVariables::TreeVariables() : leadMu(0), subMu(0), photon(0)
+ZMuMuGammaAnalysis::TreeVariables::TreeVariables() : leadMu(0), subMu(0), photon(0), mm(0), mmg(0)
 {}
 
 // ----------------------------------------------------------------------------------------------------
@@ -28,15 +33,31 @@ void ZMuMuGammaAnalysis::Init(LoopAll& l)
     l.BookExternalTreeBranch( "event",       &treevars_.event, "zmmgAnalysis" );         
     l.BookExternalTreeBranch( "lumi",        &treevars_.lumi, "zmmgAnalysis" );         
     l.BookExternalTreeBranch( "mass",        &treevars_.mass, "zmmgAnalysis" );         
+    l.BookExternalTreeBranch( "category",    &treevars_.category, "zmmgAnalysis" );
     l.BookExternalTreeBranch( "weight",      &treevars_.weight, "zmmgAnalysis" );         
     l.BookExternalTreeBranch( "nvtx",        &treevars_.nvtx, "zmmgAnalysis" );         
     l.BookExternalTreeBranch( "leadMu",      &treevars_.leadMu, "zmmgAnalysis" );         
     l.BookExternalTreeBranch( "subMu",       &treevars_.subMu, "zmmgAnalysis" );         
     l.BookExternalTreeBranch( "photon",      &treevars_.photon, "zmmgAnalysis" );         
+    l.BookExternalTreeBranch( "mm",          &treevars_.mm, "zmmgAnalysis" );         
+    l.BookExternalTreeBranch( "mmg",         &treevars_.mmg, "zmmgAnalysis" );         
     l.BookExternalTreeBranch( "idmva",       &treevars_.idmva, "zmmgAnalysis" );         
     l.BookExternalTreeBranch( "ciclevel",    &treevars_.ciclevel, "zmmgAnalysis" );         
-
+    // store photon ID MVA inputs
+    l.BookExternalTreeBranch( "r9",          &l.tmva_photonid_r9, "zmmgAnalysis" );         
+    l.BookExternalTreeBranch("sigietaieta",  &l.tmva_photonid_sieie, "zmmgAnalysis" );
+    l.BookExternalTreeBranch("scetawidth",   &l.tmva_photonid_etawidth, "zmmgAnalysis" );
+    l.BookExternalTreeBranch("scphiwidth",   &l.tmva_photonid_phiwidth, "zmmgAnalysis" );
+    l.BookExternalTreeBranch("idmva_CoviEtaiPhi",   &l.tmva_photonid_sieip, "zmmgAnalysis" );
+    l.BookExternalTreeBranch("idmva_s4ratio",   &l.tmva_photonid_s4ratio, "zmmgAnalysis" );
+    l.BookExternalTreeBranch("idmva_GammaIso",   &l.tmva_photonid_pfphotoniso03, "zmmgAnalysis" );
+    l.BookExternalTreeBranch("idmva_ChargedIso_selvtx",   &l.tmva_photonid_pfchargedisogood03, "zmmgAnalysis" );
+    l.BookExternalTreeBranch("idmva_ChargedIso_worstvtx",   &l.tmva_photonid_pfchargedisobad03, "zmmgAnalysis" );
+    l.BookExternalTreeBranch("sceta",   &l.tmva_photonid_sceta, "zmmgAnalysis" );
+    l.BookExternalTreeBranch("rho",   &l.tmva_photonid_eventrho, "zmmgAnalysis" );
+    
     l.rooContainer->BlindData(false);
+    l.rooContainer->SaveSystematicsData(true);
     
     // initialize Hgg machinery, forcing     
     StatAnalysis::Init(l);
@@ -61,6 +82,7 @@ void ZMuMuGammaAnalysis::Init(LoopAll& l)
     triggerSelections.clear();
     triggerSelections.push_back(TriggerSelection(1,-1));
     triggerSelections.back().addpath("HLT_Mu17_TkMu8");
+    triggerOnMc = true;
 }
 
 // ----------------------------------------------------------------------------------------------------
@@ -128,47 +150,53 @@ bool ZMuMuGammaAnalysis::AnalyseEvent(LoopAll& l, Int_t jentry, float weight, TL
 
         
     std::vector<int> sorted_phos;
+    std::vector<float> pho_minDr;
     TClonesArray phos_p4(TLorentzVector::Class(),l.pho_n);
     for(int ipho=0; ipho<l.pho_n; ++ipho) { 
     	    TLorentzVector p4 = l.get_pho_p4( ipho, 0, &smeared_pho_energy[0]);
     	    // Fill TClonesArray with corrected 4-vectors
     	    new(phos_p4[ipho]) TLorentzVector(p4);
 	    TVector3 & sc = *((TVector3*)l.sc_xyz->At(l.pho_scind[ipho]));
-            
+	    
+	    pho_minDr.push_back( min(p4.DeltaR(leadMu), p4.DeltaR(subMu)) );
 	    if  ( photonSelection ( p4, sc )   ) {
     		    sorted_phos.push_back(ipho);
     	    }
     }
     //cout << " After photon selection photon size  " <<  sorted_phos.size() << endl;    
     if( sorted_phos.size() < 1 ) { return false; }
-    std::sort(sorted_phos.begin(),sorted_phos.end(),
-    	      ClonesSorter<TLorentzVector,double,std::greater<double> >(&phos_p4,&TLorentzVector::Pt));
+    std::sort(sorted_phos.begin(),sorted_phos.end(),SimpleSorter<float>(&pho_minDr[0]) );
+    //// std::sort(sorted_phos.begin(),sorted_phos.end(),
+    //// 	      ClonesSorter<TLorentzVector,double,std::greater<double> >(&phos_p4,&TLorentzVector::Pt));
     
     int iselPho = sorted_phos[0];
     TLorentzVector & selPho =  *((TLorentzVector*)phos_p4.At(iselPho));
     if ( ! FSRselection (l, ileadMu, isubMu, iselPho, phos_p4  ) ) {return false;}
     //cout << " After FSR selection " << endl;            
     // Apply photon pre-selection a la Hgg
-    if (! l.PhotonMITPreSelection( iselPho, 0 , &smeared_pho_energy[0] ) ) {return false;}
+    if ( applyPhoPresel && ! l.PhotonMITPreSelection( iselPho, 0 , &smeared_pho_energy[0] ) ) {return false;}
 
     // define categories for plotting
-    int etacat = (l.pho_isEB[iselPho]);
-    int ptcat  = (selPho.Pt()>30.);
+    int etacat = (!l.pho_isEB[iselPho]);
+    int ptcat  = (selPho.Pt()<25.);
     int r9cat  = (l.pho_r9[iselPho] < 0.94);
     category = r9cat  + 2*etacat + 4*ptcat;
-      
+    mass = this->getMumugP4().M();
+    
     evweight = weight * smeared_pho_weight[iselPho] * genLevWeight;
     if( ! isSyst ) {
     	    l.countersred[diPhoCounter_]++;
-    }
 
-    // fill control plots
-    fillPlots(0,evweight,l,leadMu,subMu,diMu,iselPho,selPho,this->getMumugP4());
-    fillPlots(1+etacat,evweight,l,leadMu,subMu,diMu,iselPho,selPho,this->getMumugP4());
-    fillPlots(3+2*etacat+ptcat,evweight,l,leadMu,subMu,diMu,iselPho,selPho,this->getMumugP4());
-    fillPlots(7+category,evweight,l,leadMu,subMu,diMu,iselPho,selPho,this->getMumugP4());
-    //cout << " After filling the plots " << endl;    
-    //return (category >= 0 && mass>=massMin && mass<=massMax);
+	    // fill control plots
+	    fillPlots(0,evweight,l,leadMu,subMu,diMu,iselPho,selPho,this->getMumugP4());
+	    fillPlots(1+etacat,evweight,l,leadMu,subMu,diMu,iselPho,selPho,this->getMumugP4());
+	    fillPlots(3+2*etacat+ptcat,evweight,l,leadMu,subMu,diMu,iselPho,selPho,this->getMumugP4());
+	    fillPlots(7+category,evweight,l,leadMu,subMu,diMu,iselPho,selPho,this->getMumugP4());
+	    //cout << " After filling the plots " << endl;    
+	    //return (category >= 0 && mass>=massMin && mass<=massMax);
+    }
+    
+    treevars_.category = category;
     return (category >= 0 );
 }
 
@@ -182,29 +210,29 @@ void ZMuMuGammaAnalysis::fillPlots(int cat, float evweight,
   TVector3 & selSc = *((TVector3*)l.sc_xyz->At(l.pho_scind[iselPho]));
   double selScE =  l.sc_raw[l.pho_scind[iselPho]];
   //
-  l.FillHist("mmg_pt"       ,cat,mmg.Pt(),evweight);
-  l.FillHist("mmg_eta"      ,cat,mmg.Eta(),evweight);
-  l.FillHist("mmg_phi"      ,cat,mmg.Phi(),evweight);
-  l.FillHist("mmg_mass"     ,cat,mmg.M(),evweight);
-  l.FillHist("nvtx"     ,cat,l.vtx_std_n,evweight);
-  l.FillHist("pho_n"    ,cat,l.pho_n,evweight);
-  l.FillHist("pho_e"    ,cat,selPho.E(),evweight);
-  l.FillHist("pho_pt"   ,cat,selPho.Pt(),evweight);
-  l.FillHist("pho_eta"  ,cat,selPho.Eta(),evweight);
-  l.FillHist("pho_phi"  ,cat,selPho.Phi(),evweight);
-  l.FillHist("pho_sce"  ,cat,selScE,evweight);
-  l.FillHist("pho_sceta",cat,selSc.Eta(),evweight);
-  l.FillHist("pho_r9"   ,cat,l.pho_r9[iselPho],evweight);
-  l.FillHist("mu1_pt"   ,cat,leadMu.Pt(),evweight);
-  l.FillHist("mu1_eta"  ,cat,leadMu.Eta(),evweight);
-  l.FillHist("mu1_phi"  ,cat,leadMu.Phi(),evweight);
-  l.FillHist("mu2_pt"   ,cat,subMu.Pt(),evweight);
-  l.FillHist("mu2_eta"  ,cat,subMu.Eta(),evweight);
-  l.FillHist("mu2_phi"  ,cat,subMu.Phi(),evweight);
-  l.FillHist("mumu_pt"  ,cat,diMu.Pt(),evweight);
-  l.FillHist("mumu_eta" ,cat,diMu.Eta(),evweight);
-  l.FillHist("mumu_phi" ,cat,diMu.Phi(),evweight);
-  l.FillHist("mumu_mass",cat,diMu.M(),evweight);
+  l.FillHist("mmg_pt"       	,cat,mmg.Pt(),evweight);
+  l.FillHist("mmg_eta"      	,cat,mmg.Eta(),evweight);
+  l.FillHist("mmg_phi"      	,cat,mmg.Phi(),evweight);
+  l.FillHist("mmg_mass"     	,cat,mmg.M(),evweight);
+  l.FillHist("nvtx"     	,cat,l.vtx_std_n,evweight);
+  l.FillHist("pho_n"    	,cat,l.pho_n,evweight);
+  l.FillHist("pho_e"    	,cat,selPho.E(),evweight);
+  l.FillHist("pho_pt"   	,cat,selPho.Pt(),evweight);
+  l.FillHist("pho_eta"  	,cat,selPho.Eta(),evweight);
+  l.FillHist("pho_phi"  	,cat,selPho.Phi(),evweight);
+  l.FillHist("pho_sce"  	,cat,selScE,evweight);
+  l.FillHist("pho_sceta"	,cat,selSc.Eta(),evweight);
+  l.FillHist("pho_r9"   	,cat,l.pho_r9[iselPho],evweight);
+  l.FillHist("mu1_pt"   	,cat,leadMu.Pt(),evweight);
+  l.FillHist("mu1_eta"  	,cat,leadMu.Eta(),evweight);
+  l.FillHist("mu1_phi"  	,cat,leadMu.Phi(),evweight);
+  l.FillHist("mu2_pt"   	,cat,subMu.Pt(),evweight);
+  l.FillHist("mu2_eta"  	,cat,subMu.Eta(),evweight);
+  l.FillHist("mu2_phi"  	,cat,subMu.Phi(),evweight);
+  l.FillHist("mumu_pt"  	,cat,diMu.Pt(),evweight);
+  l.FillHist("mumu_eta" 	,cat,diMu.Eta(),evweight);
+  l.FillHist("mumu_phi" 	,cat,diMu.Phi(),evweight);
+  l.FillHist("mumu_mass"	,cat,diMu.M(),evweight);
   
   if( cat == 0 ) { 
     treevars_.run       = l.run;
@@ -216,6 +244,8 @@ void ZMuMuGammaAnalysis::fillPlots(int cat, float evweight,
     *(treevars_.leadMu) = leadMu;
     *(treevars_.subMu)  = subMu;
     *(treevars_.photon) = selPho;
+    *(treevars_.mm)     = diMu;
+    *(treevars_.mmg)    = mmg;
     treevars_.idmva     = l.photonIDMVA(iselPho,0,selPho,bdtTrainingType.c_str());
     std::vector<std::vector<bool> > ph_passcut;
     treevars_.ciclevel  = l.PhotonCiCPFSelectionLevel(iselPho, 0, ph_passcut, 4, 0, &smeared_pho_energy[0]);
@@ -225,20 +255,18 @@ void ZMuMuGammaAnalysis::fillPlots(int cat, float evweight,
 bool ZMuMuGammaAnalysis::muonSelection(LoopAll& l, int iMu) {
   bool result=true;
   TLorentzVector * p4 = (TLorentzVector*)l.mu_glo_p4->At(iMu);
-
- 
-
+  
   //  cout << " entering Muon Selection Pt " <<  p4->Pt() << " tkLay " <<  l.mu_tkLayers[iMu] << " innerhits " << l.mu_glo_innerhits[iMu] << " Pixel " <<  l.mu_glo_pixelhits[iMu] << " valid chamb " <<  l.mu_glo_validChmbhits[iMu] << " nMatches " << l.mu_glo_nmatches[iMu] << " chi2 " <<l.mu_glo_chi2[iMu]/l.mu_glo_dof[iMu] << " d0 " <<   l.mu_glo_D0Vtx[iMu][0] << " dz " << l.mu_glo_DZVtx[iMu][0] << " gsf " << l.mu_glo_hasgsftrack[iMu] << endl;
   //cout << " cut values " << muPtMin << " " << muTkLayers << " " << muPixelHits << " " << muValidChambers << " " << muNmatches << " " << muNormChi2 << " " << muD0Vtx << " " << muDZVtx << endl;
 
   if ( p4->Pt() <  muPtMin )                                     result=false;  
   if ( l.mu_tkLayers[iMu] <= muTkLayers  )                       result=false;
-  if ( l.mu_glo_pixelhits[iMu] < muPixelHits )                   result=false;
-  if ( l.mu_glo_validChmbhits[iMu] < muValidChambers )           result=false;
+  if ( l.mu_glo_pixelhits[iMu] <= muPixelHits )                   result=false;
+  if ( l.mu_glo_validChmbhits[iMu] <= muValidChambers )           result=false;
   if ( l.mu_glo_nmatches[iMu] <= muNmatches  )                   result=false;
   if ( l.mu_glo_chi2[iMu]/l.mu_glo_dof[iMu] >= muNormChi2  )     result=false;
-  if ( l.mu_glo_D0Vtx[iMu][0] > muD0Vtx )                        result=false;
-  if ( l.mu_glo_DZVtx[iMu][0] > muDZVtx )                        result=false;
+  if ( l.mu_glo_D0Vtx[iMu][0] >= muD0Vtx )                        result=false;
+  if ( l.mu_glo_DZVtx[iMu][0] >= muDZVtx )                        result=false;
   if ( l.mu_glo_hasgsftrack[iMu] )                               result=false;
 
   //cout << " ending Muon Selection result " << result <<  endl;
@@ -252,15 +280,13 @@ bool ZMuMuGammaAnalysis::photonSelection (TLorentzVector& p4, TVector3 & sc ) {
   bool result=true;
   if ( fabs(sc.Eta())  > 2.5 )                               result=false;
   if ( fabs(sc.Eta()) > 1.4442 &&  fabs(sc.Eta())  < 1.566 ) result=false;
-  if ( p4.Pt() < 10)                                         result=false;
-
+  if ( p4.Pt() < phoPtMin)                                   result=false;
+  
   return result;
 
 }
 
 bool ZMuMuGammaAnalysis::FSRselection ( LoopAll& l, int ileadMu, int isubMu, int iPho, TClonesArray& phos_p4 ) {
-
-  bool result=true;
 
   int iNearMu=ileadMu;
   int iFarMu=isubMu;
@@ -268,11 +294,14 @@ bool ZMuMuGammaAnalysis::FSRselection ( LoopAll& l, int ileadMu, int isubMu, int
   TLorentzVector & leadMu =  *( (TLorentzVector*)l.mu_glo_p4->At(ileadMu));
   TLorentzVector & subMu  =  *( (TLorentzVector*)l.mu_glo_p4->At(isubMu) );    
   TLorentzVector & selPho =  *((TLorentzVector*)phos_p4.At(iPho));
+  TLorentzVector diMu = leadMu + subMu;
+  
+  if (leadMu.Pt() < 30. || diMu.M() < 45.  ){ return false; }
   //
-  float leadMuDPhi = l.DeltaPhi ( leadMu.Phi(), selPho.Phi() );
-  float subleadMuDPhi = l.DeltaPhi ( leadMu.Phi(), selPho.Phi() );
-  float leadMuDEta =  leadMu.Eta() -  selPho.Eta();
-  float subleadMuDEta =  subMu.Eta() -  selPho.Eta();
+  float leadMuDPhi = leadMu.DeltaPhi(selPho);
+  float subleadMuDPhi = subMu.DeltaPhi(selPho);
+  float leadMuDEta =  fabs(leadMu.Eta() - selPho.Eta());
+  float subleadMuDEta =  fabs(subMu.Eta() - selPho.Eta());
   //
   float leadMuDR = sqrt(leadMuDEta*leadMuDEta + leadMuDPhi*leadMuDPhi );
   float subleadMuDR = sqrt(subleadMuDEta*subleadMuDEta + subleadMuDPhi*subleadMuDPhi );
@@ -283,21 +312,21 @@ bool ZMuMuGammaAnalysis::FSRselection ( LoopAll& l, int ileadMu, int isubMu, int
   }
   //
   float minDr = min(leadMuDR, subleadMuDR);
-  if ( minDr > 0.8) result=false;
+  if ( minDr > 0.8) { return false; };
+  float minDeta = min(leadMuDEta, subleadMuDEta);
+  if ( minDeta < dEtaMin ) { return false; };
   // apply isolation on the muons
-  if ( l.mu_glo_chhadiso04[ileadMu]/leadMu.Pt() > 0.2 ) result=false;
-  if ( l.mu_glo_chhadiso04[isubMu]/subMu.Pt()   > 0.2 ) result=false;
+  if ( l.mu_glo_chhadiso04[ileadMu]/leadMu.Pt() > 0.2 ) { return false; };
+  if ( l.mu_glo_chhadiso04[isubMu]/subMu.Pt()   > 0.2 ) { return false; };
   //
   TLorentzVector & farMuP4 =  *( (TLorentzVector*)l.mu_glo_p4->At(iFarMu));
 
-  TLorentzVector diMu = leadMu + subMu;
   mumugMass_ = diMu + selPho; 
-
-
-  if ( farMuP4.Pt() < 21) result=false;
-  if ( mumugMass_.M() < massMin || mumugMass_.M() > massMax ) result=false;
-  if ( (mumugMass_+diMu).M() > 180 ) result=false;
-
-  return result;
+    
+  if ( farMuP4.Pt() < 21.) { return false; };
+  if ( mumugMass_.M() < massMin || mumugMass_.M() > massMax ) { return false; };
+  if ( mumugMass_.M()+diMu.M() > 180. ) { return false; };
+  
+  return true;
 
 }
